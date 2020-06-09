afa1d9617f9412f03e0d4b46f553f37361b39279
Larpx <DLarpx@hotmail.com>
2020-06-09 21:42:28 +0800

    注销无用代码

--- BaseLibrary/Web/HttpsHelper.cs
+++ BaseLibrary/Web/HttpsHelper.cs
@@ -1050,173 +1050,172 @@ namespace Larpx.ResourceSpider.CommonHelper
 
     public class httph
     {
-        private Uri oUri;
-        private int nTimeout = 6 * 1000;
-        private int nLoopTimes = 0;
-        private CookieContainer oCookieContainer;
+        //private Uri oUri;
+        //private int nTimeout = 6 * 1000;
+        //private int nLoopTimes = 0;
+        //private CookieContainer oCookieContainer;
 
-        /// <summary>
-        /// 资源地址
-        /// </summary>
-        public Uri Uri { get => oUri; set => oUri = value; }
+        ///// <summary>
+        ///// 资源地址
+        ///// </summary>
+        //public Uri Uri { get => oUri; set => oUri = value; }
 
-        /// <summary>
-        /// 请求时所携带的Cookie
-        /// </summary>
-        public CookieContainer CookieContainer { get => oCookieContainer; set => oCookieContainer = value; }
+        ///// <summary>
+        ///// 请求时所携带的Cookie
+        ///// </summary>
+        //public CookieContainer CookieContainer { get => oCookieContainer; set => oCookieContainer = value; }
 
-        /// <summary>
-        /// 重复请求次数
-        /// </summary>
-        public int LoopTimes { get => nLoopTimes; set => nLoopTimes = value; }
+        ///// <summary>
+        ///// 重复请求次数
+        ///// </summary>
+        //public int LoopTimes { get => nLoopTimes; set => nLoopTimes = value; }
 
-        /// <summary>
-        /// 请求超时时间，ms
-        /// </summary>
-        public int NTimeout { get => nTimeout; set => nTimeout = value; }
+        ///// <summary>
+        ///// 请求超时时间，ms
+        ///// </summary>
+        //public int NTimeout { get => nTimeout; set => nTimeout = value; }
 
-        public httph(CookieContainer oCookieContainer)
-        {
-            this.CookieContainer = oCookieContainer;
-        }
+        //public httph(CookieContainer oCookieContainer)
+        //{
+        //    this.CookieContainer = oCookieContainer;
+        //}
 
-        public httph()
-        {
-            this.CookieContainer = new CookieContainer();
-            WebClient webClient = new WebClient();
+        //public httph()
+        //{
+        //    this.CookieContainer = new CookieContainer();
+        //    WebClient webClient = new WebClient();
             
-        }
+        //}
 
-        /// <summary>
-        /// Get
-        /// 获取数据,需要判断返回结果是否为空
-        /// </summary>
-        /// <param name="url">Uri资源</param>
-        /// <param name="sAccept">Accept头</param>
-        /// <param name="sUserAgent">UserAgent头</param>
-        /// <param name="oWebHeaderCollection">请求头信息</param>
-        /// <param name="oCookieContainer">Cookie容器</param>
-        /// <returns>响应信息</returns>
-        public static HttpWebResponse ReadData(string url, int n = 0, string sRedisKey = null,
-            string sAccept = null, string sUserAgent = null)
-        {
-            int nSign = 4;
-            try
-            {
-                if (n >= nSign)
-                    return null;
+        ///// <summary>
+        ///// Get
+        ///// 获取数据,需要判断返回结果是否为空
+        ///// </summary>
+        ///// <param name="url">Uri资源</param>
+        ///// <param name="sAccept">Accept头</param>
+        ///// <param name="sUserAgent">UserAgent头</param>
+        ///// <param name="oWebHeaderCollection">请求头信息</param>
+        ///// <param name="oCookieContainer">Cookie容器</param>
+        ///// <returns>响应信息</returns>
+        //public static HttpWebResponse ReadData(string url, int n = 0, string sRedisKey = null,
+        //    string sAccept = null, string sUserAgent = null)
+        //{
+        //    int nSign = 4;
+        //    try
+        //    {
+        //        if (n >= nSign)
+        //            return null;
 
-                Uri oURL = new Uri(url);
-                Random oRand = new Random();
-                string sRdsKey = string.IsNullOrEmpty(sRedisKey) ? oURL.Host : sRedisKey;
-                StackExchangeRedisHelper oRedisCookie = new StackExchangeRedisHelper(nRedisCookieDB);
+        //        Uri oURL = new Uri(url);
+        //        Random oRand = new Random();
+        //        string sRdsKey = string.IsNullOrEmpty(sRedisKey) ? oURL.Host : sRedisKey;
+        //        StackExchangeRedisHelper oRedisCookie = new StackExchangeRedisHelper(nRedisCookieDB);
 
-                //设置https验证方式
-                if (url.ToString().StartsWith("https", StringComparison.OrdinalIgnoreCase))
-                {
-                    ServicePointManager.ServerCertificateValidationCallback =
-                            new RemoteCertificateValidationCallback(CheckValidationResult);
-                }
+        //        //设置https验证方式
+        //        if (url.ToString().StartsWith("https", StringComparison.OrdinalIgnoreCase))
+        //        {
+        //            ServicePointManager.ServerCertificateValidationCallback =
+        //                    new RemoteCertificateValidationCallback(CheckValidationResult);
+        //        }
 
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        //        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 
-                request.AllowAutoRedirect = true;
+        //        request.AllowAutoRedirect = true;
 
-                //获取Cookie，从Redis
-                if (oRedisCookie.IsSet(sRdsKey))
-                {
-                    var oCookies = CookieHelper.GetCookiesByHeader(oRedisCookie.Get<string>(sRdsKey));
-                    request.CookieContainer.Add(oCookies);
-                }
-                else
-                    request.CookieContainer.Add(GetCookie(oURL, true, oURL.Host, 5));
+        //        //获取Cookie，从Redis
+        //        if (oRedisCookie.IsSet(sRdsKey))
+        //        {
+        //            var oCookies = CookieHelper.GetCookiesByHeader(oRedisCookie.Get<string>(sRdsKey));
+        //            request.CookieContainer.Add(oCookies);
+        //        }
+        //        else
+        //            request.CookieContainer.Add(GetCookie(oURL, true, oURL.Host, 5));
 
-                if (!String.IsNullOrEmpty(sAccept))
-                    request.Accept = sAccept;
-                else
-                    request.Accept = @"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-                if (!String.IsNullOrEmpty(sUserAgent))
-                    request.UserAgent = sUserAgent;
-                else
-                    request.UserAgent = GetRandomUserAgent();
-                request.Timeout = 20 * 1000;
-                request.Headers.Add(HttpRequestHeader.CacheControl, @"no-cache");
-                request.Headers.Add("upgrade-insecure-requests", "1");
-                request.Headers.Add(HttpRequestHeader.Pragma, "no-cache");
-                request.Headers.Add(HttpRequestHeader.AcceptEncoding, @"gzip, deflate, br");
-                request.Referer = new Uri(url).Host.ToString();
+        //        if (!String.IsNullOrEmpty(sAccept))
+        //            request.Accept = sAccept;
+        //        else
+        //            request.Accept = @"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+        //        if (!String.IsNullOrEmpty(sUserAgent))
+        //            request.UserAgent = sUserAgent;
+        //        else
+        //            request.UserAgent = GetRandomUserAgent();
+        //        request.Timeout = 20 * 1000;
+        //        request.Headers.Add(HttpRequestHeader.CacheControl, @"no-cache");
+        //        request.Headers.Add("upgrade-insecure-requests", "1");
+        //        request.Headers.Add(HttpRequestHeader.Pragma, "no-cache");
+        //        request.Headers.Add(HttpRequestHeader.AcceptEncoding, @"gzip, deflate, br");
+        //        request.Referer = new Uri(url).Host.ToString();
 
-                //sleep
-                int nRand = oRand.Next(20, 80);
-                Console.WriteLine("Net Request Sleeping: " + nRand + "ms");
-                HttpWebResponse respone = (HttpWebResponse)request.GetResponse();
-                if (respone.StatusCode == HttpStatusCode.OK)
-                    return respone;
-                else if (respone.StatusCode > HttpStatusCode.InternalServerError)
-                    return ReadData(url, n++, sRedisKey, sAccept, sUserAgent);
-                else
-                    return ReadData(url, n++, sRedisKey, sAccept, sUserAgent);
-            }
-            catch (IOException e)
-            {
-                if (n > nSign)
-                    throw e;
-                else
-                {
-                    n++;
-                    return ReadData(url, n, sRedisKey, sAccept, sUserAgent);
-                }
-            }
-            catch (WebException e)
-            {
-                if (e.Message.Contains("40") || e.Message.Contains("50"))
-                {
-                    return null;
-                }
-                else
-                {
-                    if (n > nSign)
-                        throw e;
-                    else
-                    {
-                        n++;
-                        return ReadData(url, n, sRedisKey, sAccept, sUserAgent);
-                    }
-                }
+        //        //sleep
+        //        int nRand = oRand.Next(20, 80);
+        //        Console.WriteLine("Net Request Sleeping: " + nRand + "ms");
+        //        HttpWebResponse respone = (HttpWebResponse)request.GetResponse();
+        //        if (respone.StatusCode == HttpStatusCode.OK)
+        //            return respone;
+        //        else if (respone.StatusCode > HttpStatusCode.InternalServerError)
+        //            return ReadData(url, n++, sRedisKey, sAccept, sUserAgent);
+        //        else
+        //            return ReadData(url, n++, sRedisKey, sAccept, sUserAgent);
+        //    }
+        //    catch (IOException e)
+        //    {
+        //        if (n > nSign)
+        //            throw e;
+        //        else
+        //        {
+        //            n++;
+        //            return ReadData(url, n, sRedisKey, sAccept, sUserAgent);
+        //        }
+        //    }
+        //    catch (WebException e)
+        //    {
+        //        if (e.Message.Contains("40") || e.Message.Contains("50"))
+        //        {
+        //            return null;
+        //        }
+        //        else
+        //        {
+        //            if (n > nSign)
+        //                throw e;
+        //            else
+        //            {
+        //                n++;
+        //                return ReadData(url, n, sRedisKey, sAccept, sUserAgent);
+        //            }
+        //        }
 
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("无效的控制字符"))
-                {
-                    if (n > nSign)
-                        throw ex;
-                    else
-                    {
-                        n++;
-                        return ReadData(url, n, sRedisKey, sAccept, sUserAgent);
-                    }
-                }
-                else
-                    throw ex;
-            }
-        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        if (ex.Message.Contains("无效的控制字符"))
+        //        {
+        //            if (n > nSign)
+        //                throw ex;
+        //            else
+        //            {
+        //                n++;
+        //                return ReadData(url, n, sRedisKey, sAccept, sUserAgent);
+        //            }
+        //        }
+        //        else
+        //            throw ex;
+        //    }
+        //}
 
-        /// <summary>
-        /// 检查证书，适用于HTTPS
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="certificate"></param>
-        /// <param name="chain"></param>
-        /// <param name="errors"></param>
-        /// <returns></returns>
-        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
-        {
-            return true; //总是接受   
-        }
+        ///// <summary>
+        ///// 检查证书，适用于HTTPS
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="certificate"></param>
+        ///// <param name="chain"></param>
+        ///// <param name="errors"></param>
+        ///// <returns></returns>
+        //private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        //{
+        //    return true; //总是接受   
+        //}
 
     }
-
 
     /// <summary>
     /// 基于HttpClient实现的Http帮助类
