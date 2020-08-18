75b2a1fcc1abcf1724e9eef7edc7fc32e4bf3617
Larpx <237198606@qq.com>
2020-08-18 15:55:18 +0800

    接口

---
+++ Larpx.ResourceSpider.Http/Downloader/IDownloader.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Larpx.ResourceSpider.Http.Downloader
+{
+    public interface IDownloader
+    {
+        Task<Response> DownloadAsync(Request request);
+    }
+}
